67010948808dd9ac5dc1d16d9d75c2728aa86df1
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-11-23 02:25:54 +0200

    SCRUM-36 implement flight viewing feature

--- backend/src/main/java/com/example/backend/Entities/Flight.java
+++ backend/src/main/java/com/example/backend/Entities/Flight.java
@@ -1,5 +1,58 @@
 package com.example.backend.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String source;
     
+    @Column(nullable = false)
+    private String destination;
+    
+    @Column(nullable = false)
+    private LocalDate arrivalDate;
+    
+    @Column(nullable = false)
+    private LocalDate departureDate;
+    
+    @Column(nullable = false)
+    private LocalTime arrivalTime;
+
+    @Column(nullable = false)
+    private LocalTime departureTime;
+    
+    @Column(nullable = false)
+    private float economyPrice;
+    
+    @Column(nullable = false)
+    private float businessPrice;
+    
+    @Column(nullable = false)
+    private int availableEconomySeats;
+    
+    @Column(nullable = false)
+    private int availableBusinessSeats;
+    
+    private boolean isCancel;
 }
---
+++ backend/src/test/java/com/example/backend/Repositories/FlightRepositoryTest.java
@@ -0,0 +1,99 @@
+package com.example.backend.Repositories;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import com.example.backend.Entities.Flight;
+
+@DataJpaTest
+public class FlightRepositoryTest {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @Test
+    void testFindByDepartureDateCorrectence() {
+        // given
+        String[] source = { "New York", "Cairo", "Miami", "New York", "San Fransico", "Cairo" };
+        String[] destination = { "Miami", "Aswan", "New York", "Miami", "Miami", "Alexandria" };
+        LocalDate arrivalDate = LocalDate.parse("2024-10-12");
+        LocalTime arrivalTime = LocalTime.parse("10:12:00");
+        LocalTime departureTime = LocalTime.parse("10:12:00");
+        int availableBusinessSeats = 10;
+        int availableEconomySeats = 50;
+        float economyPrice = 200;
+        float businessPrice = 2000;
+
+        LocalDate[] date = {
+                LocalDate.parse("2024-10-12"), LocalDate.parse("2024-09-12"),
+                LocalDate.parse("2024-08-12"), LocalDate.parse("2024-10-11"),
+                LocalDate.parse("2025-10-12"), LocalDate.parse("2024-10-12") };
+
+        List<Flight> actual = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            Flight flight = Flight.builder()
+                    .source(source[i])
+                    .destination(destination[i])
+                    .departureDate(date[i])
+                    .arrivalDate(arrivalDate)
+                    .arrivalTime(arrivalTime)
+                    .departureTime(departureTime)
+                    .economyPrice(economyPrice)
+                    .businessPrice(businessPrice)
+                    .availableEconomySeats(availableEconomySeats)
+                    .availableBusinessSeats(availableBusinessSeats)
+                    .build();
+
+            flightRepository.save(flight);
+
+            if (i == 0 || i == 5)
+                actual.add(flight);
+        }
+
+        // where
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Flight> page = flightRepository.findByDepartureDate(LocalDate.parse("2024-10-12"), pageable);
+
+        // then
+        Assertions.assertEquals(page.getContent(), actual);
+
+    }
+
+    @Test
+    void testFindByDepartureDateWhenNotFound() {
+        // given
+        List<Flight> actual = new ArrayList<>();
+
+        // where
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Flight> page = flightRepository.findByDepartureDate(LocalDate.parse("2024-10-12"), pageable);
+
+        // then
+        Assertions.assertEquals(page.getContent(), actual);
+    }
+
+    @Test
+    void testFindByDepartureDateWhen() {
+        // given
+        List<Flight> actual = new ArrayList<>();
+
+        // where
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Flight> page = flightRepository.findByDepartureDate(LocalDate.parse("2024-10-12"), pageable);
+
+        // then
+        Assertions.assertEquals(page.getContent(), actual);
+    }
+    // if search for page that not exist
+    // if the departure date was null or not date
+}
