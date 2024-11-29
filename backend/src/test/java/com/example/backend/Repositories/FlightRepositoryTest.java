7802b1af576be7272afd40b89a696da22a89d699
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-11-29 12:43:05 +0200

    add tests

--- backend/src/test/java/com/example/backend/Repositories/FlightRepositoryTest.java
+++ backend/src/test/java/com/example/backend/Repositories/FlightRepositoryTest.java
@@ -4,27 +4,51 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.Rollback;
+
 import com.example.backend.Entities.Flight;
 
 @DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class FlightRepositoryTest {
 
     @Autowired
-    private FlightRepository flightRepository;
+    private FlightRepository underTest;
+
+    private final LocalDate DEPARTURE_DATE = LocalDate.parse("2024-10-12");
 
     @Test
+    @Order(1)
+    void testFindByDepartureDateWhenDatabaseIsEmpty() {
+        // given
+        int pageSize = 10;
+        int pageNumber = 0;
+
+        // where
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<Flight> page = underTest.findByDepartureDate(DEPARTURE_DATE, pageable);
+
+        // then
+        Assertions.assertTrue(page.isEmpty());
+    }
+
+    @Test
+    @Rollback(value = false)
+    @Order(2)
     void testFindByDepartureDateCorrectence() {
         // given
-        String[] source = { "New York", "Cairo", "Miami", "New York", "San Fransico", "Cairo" };
-        String[] destination = { "Miami", "Aswan", "New York", "Miami", "Miami", "Alexandria" };
+        String source = "New York";
+        String destination = "Miami";
         LocalDate arrivalDate = LocalDate.parse("2024-10-12");
         LocalTime arrivalTime = LocalTime.parse("10:12:00");
         LocalTime departureTime = LocalTime.parse("10:12:00");
@@ -38,12 +62,15 @@ public class FlightRepositoryTest {
                 LocalDate.parse("2024-08-12"), LocalDate.parse("2024-10-11"),
                 LocalDate.parse("2025-10-12"), LocalDate.parse("2024-10-12") };
 
-        List<Flight> actual = new ArrayList<>();
+        List<Flight> expected = new ArrayList<>();
+
+        int pageSize = 10;
+        int pageNumber = 0;
 
         for (int i = 0; i < 6; i++) {
             Flight flight = Flight.builder()
-                    .source(source[i])
-                    .destination(destination[i])
+                    .source(source)
+                    .destination(destination)
                     .departureDate(date[i])
                     .arrivalDate(arrivalDate)
                     .arrivalTime(arrivalTime)
@@ -54,46 +81,35 @@ public class FlightRepositoryTest {
                     .availableBusinessSeats(availableBusinessSeats)
                     .build();
 
-            flightRepository.save(flight);
+            underTest.save(flight);
 
             if (i == 0 || i == 5)
-                actual.add(flight);
+                expected.add(flight);
         }
 
         // where
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<Flight> page = flightRepository.findByDepartureDate(LocalDate.parse("2024-10-12"), pageable);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<Flight> page = underTest.findByDepartureDate(LocalDate.parse("2024-10-12"), pageable);
 
         // then
-        Assertions.assertEquals(page.getContent(), actual);
+        Assertions.assertEquals(expected, page.getContent());
+        Assertions.assertEquals(2, page.getNumberOfElements());
 
     }
 
     @Test
-    void testFindByDepartureDateWhenNotFound() {
+    @Order(3)
+    void testFindByDepartureDateWhenPageIsNotFound() {
         // given
-        List<Flight> actual = new ArrayList<>();
+        int pageSize = 10;
+        int pageNumber = 1;
 
         // where
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<Flight> page = flightRepository.findByDepartureDate(LocalDate.parse("2024-10-12"), pageable);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<Flight> page = underTest.findByDepartureDate(DEPARTURE_DATE, pageable);
 
         // then
-        Assertions.assertEquals(page.getContent(), actual);
+        Assertions.assertTrue(page.isEmpty());
     }
 
-    @Test
-    void testFindByDepartureDateWhen() {
-        // given
-        List<Flight> actual = new ArrayList<>();
-
-        // where
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<Flight> page = flightRepository.findByDepartureDate(LocalDate.parse("2024-10-12"), pageable);
-
-        // then
-        Assertions.assertEquals(page.getContent(), actual);
-    }
-    // if search for page that not exist
-    // if the departure date was null or not date
 }
--- backend/src/test/java/com/example/backend/Services/FlightDisplayServiceTest.java
+++ backend/src/test/java/com/example/backend/Services/FlightDisplayServiceTest.java
@@ -1,10 +1,111 @@
 package com.example.backend.Services;
 
-import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.verifyNoInteractions;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.Rollback;
+
+import com.example.backend.DTOMappers.AdminFlightMapper;
+import com.example.backend.DTOs.AdminFlightDTO;
+import com.example.backend.DTOs.PageResponse;
+import com.example.backend.Entities.Flight;
+import com.example.backend.Repositories.FlightRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class FlightDisplayServiceTest {
-    
+
+    @Mock
+    private FlightRepository flightRepository;
+    @Mock
+    private AdminFlightMapper adminFlightMapper;
+
+    @InjectMocks
     private FlightDisplayService flightService;
+
+    private final LocalDate DEPARTURE_DATE = LocalDate.parse("2024-12-02");
+    
+    @BeforeAll
+    @Rollback(value = false)
+    public void setup(){
+        String[] source = { "New York", "Cairo", "Miami", "New York", "San Fransico", "Cairo" };
+        String[] destination = { "Miami", "Aswan", "New York", "Miami", "Miami", "Alexandria" };
+        LocalDate arrivalDate = LocalDate.parse("2024-10-12");
+        LocalTime arrivalTime = LocalTime.parse("10:12:00");
+        LocalTime departureTime = LocalTime.parse("10:12:00");
+        int availableBusinessSeats = 10;
+        int availableEconomySeats = 50;
+        float economyPrice = 200;
+        float businessPrice = 2000;
+
+        LocalDate[] date = {
+                LocalDate.parse("2024-10-12"), LocalDate.parse("2024-09-12"),
+                LocalDate.parse("2024-08-12"), LocalDate.parse("2024-10-11"),
+                LocalDate.parse("2025-10-12"), LocalDate.parse("2024-10-12") };
+
+        List<Flight> expected = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            Flight flight = Flight.builder()
+                    .source(source[i])
+                    .destination(destination[i])
+                    .departureDate(date[i])
+                    .arrivalDate(arrivalDate)
+                    .arrivalTime(arrivalTime)
+                    .departureTime(departureTime)
+                    .economyPrice(economyPrice)
+                    .businessPrice(businessPrice)
+                    .availableEconomySeats(availableEconomySeats)
+                    .availableBusinessSeats(availableBusinessSeats)
+                    .build();
+
+            if (i == 0 || i == 5)
+                expected.add(flight);
+        }
+
+    }
+
+    @Test
+    void testGetFlightsWhenDepartureDateIsNull() {
+        // given
+        LocalDate deparuterDate = null;
+        // when
+        Exception exception = Assertions.assertThrows(NullPointerException.class,
+                () -> flightService.getFlights(deparuterDate, 0));
+        // then
+        Assertions.assertEquals("Departure date can't be null", exception.getMessage());
+        verifyNoInteractions(flightRepository);
+    }
+
+    @Test
+    void testGetFlightsWhenPageNumberIsNegative() {
+        // given
+        int pageNumber = -5;
+
+        // when
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> flightService.getFlights(DEPARTURE_DATE, pageNumber));
+
+        // then
+        Assertions.assertEquals("Page number can't be negative", exception.getMessage());
+        verifyNoInteractions(flightRepository);
+    }
+
+    @Test
+    void testGetFlightsCorrectence() {
+        // given
+         
+    }
 
     @Test
     void testFilterFlights() {
@@ -13,8 +114,7 @@ public class FlightDisplayServiceTest {
         // then (compare with expected)
     }
 
-    @Test
-    void testGetFlights() {
-
-    }
+    // test if pages or invaild
+    // test if any parameter is invaild
+    //
 }
