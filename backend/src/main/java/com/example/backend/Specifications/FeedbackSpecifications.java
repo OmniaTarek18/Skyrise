66777a00bdfeb0d944805959c26df93707278266
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-11-30 14:35:05 +0200

    SCRUM-43 handle specification for null and handle null in sort in service

--- backend/src/main/java/com/example/backend/Specifications/FeedbackSpecifications.java
+++ backend/src/main/java/com/example/backend/Specifications/FeedbackSpecifications.java
@@ -9,27 +9,27 @@ import com.example.backend.Enums.Stars;
 public class FeedbackSpecifications {
 
     public static Specification<Feedback> containsStars(Stars stars) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("stars"), stars);
+        return (root, query, criteriaBuilder) -> stars == null ? null : criteriaBuilder.equal(root.get("stars"), stars);
     }
 
     public static Specification<Feedback> containsService(QualityRating service) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("service"), service);
+        return (root, query, criteriaBuilder) -> service == null ? null : criteriaBuilder.equal(root.get("service"), service);
     }
 
     public static Specification<Feedback> containsComfort(QualityRating comfort) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("comfort"), comfort);
+        return (root, query, criteriaBuilder) -> comfort == null ? null : criteriaBuilder.equal(root.get("comfort"), comfort);
     }
 
     public static Specification<Feedback> containsPunctuality(QualityRating punctuality) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("punctuality"), punctuality);
+        return (root, query, criteriaBuilder) -> punctuality == null ? null : criteriaBuilder.equal(root.get("punctuality"), punctuality);
     }
     
     public static Specification<Feedback> containsFoodAndBeverage(QualityRating foodAndBeverage) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("foodAndBeverage"), foodAndBeverage);
+        return (root, query, criteriaBuilder) -> foodAndBeverage == null ? null : criteriaBuilder.equal(root.get("foodAndBeverage"), foodAndBeverage);
     }
     
     public static Specification<Feedback> containsCleanliness(QualityRating cleanliness) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("cleanliness"), cleanliness);
+        return (root, query, criteriaBuilder) -> cleanliness == null ? null : criteriaBuilder.equal(root.get("cleanliness"), cleanliness);
     }
 
 }
