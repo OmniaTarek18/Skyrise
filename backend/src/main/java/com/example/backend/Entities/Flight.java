2df2e5ce84f986f7d777687c561700fbb676f124
Rawan Moh Said <roro.m.said2@gmail.com>
2024-11-30 21:44:26 +0200

    SCRUM-54 Entities required are added

--- backend/src/main/java/com/example/backend/Entities/Flight.java
+++ backend/src/main/java/com/example/backend/Entities/Flight.java
@@ -1,5 +1,53 @@
 package com.example.backend.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     
+    @Column(nullable = false)
+    private LocalDate arrivalDate;
+    
+    @Column(nullable = false)
+    private LocalDate departureDate;
+    
+    @Column(nullable = false)
+    private float economyPrice;
+    
+    @Column(nullable = false)
+    private float businessPrice;
+    
+    @Column(nullable = false)
+    private int availableEconomySeats;
+    
+    @Column(nullable = false)
+    private int availableBusinessSeats;
+    
+    private boolean isCancel;
+
+    @OneToMany(mappedBy = "flight")
+    private List<FlightLeg> flightLegs;
+
+    @OneToMany(mappedBy = "flight")
+    private List<Reservation> reservations;
 }
