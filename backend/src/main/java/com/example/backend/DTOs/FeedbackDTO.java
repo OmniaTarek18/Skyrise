1dd5d4028944d6cad6a4f1e818b3be9e2d65638e
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-11-30 11:40:07 +0200

    SCRUM-43 update DTO and Mapper

--- backend/src/main/java/com/example/backend/DTOs/FeedbackDTO.java
+++ backend/src/main/java/com/example/backend/DTOs/FeedbackDTO.java
@@ -1,17 +1,20 @@
 package com.example.backend.DTOs;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.example.backend.Enums.QualityRating;
 import com.example.backend.Enums.Stars;
 
 public record FeedbackDTO(
+        Integer userId,
+        Integer flightId,
         Stars stars,
         String comment,
         String userName,
         String flightSource,
         String flightDestination,
-        String flightDepartureDate,
+        LocalDate flightDepartureDate,
         QualityRating comfort,
         QualityRating service,
         QualityRating punctuality,
