112918c11e8020cf79bfe60a6210344abc6d584a
Rana Mohamed <121067602+RanaMohamed6720@users.noreply.github.com>
2024-12-01 20:39:31 +0200

    refactoring and enhancing ui of feedback page

--- frontend/src/pages/AdminDashboard/Feedback.jsx
+++ frontend/src/pages/AdminDashboard/Feedback.jsx
@@ -1,31 +1,44 @@
 import React, { useState } from "react";
 import { Star, Filter, X, StarIcon } from "lucide-react";
+import ReviewCard from "../../components/adminDashboard/Review/ReviewCard";
 import "./feedback.css";
+import { ReviewStatCard } from "../../components/adminDashboard/Review/ReviewStatCard";
 
 const Feedback = () => {
   // mock data for feedback
   const feedback = [
     {
       id: 1,
+      userName: "nada",
+      source: "alex",
+      destination: "london",
+      date: "2024-11-15",
       starRating: 4,
-      punctuality: "Excellent",
-      comfort: "Fair",
-      service: "Good",
-      foodAndBeverage: "Fair",
-      cleanliness: "Excellent",
+      ratings: {
+        punctuality: "Excellent",
+        comfort: "Fair",
+        service: "Good",
+        foodAndBeverage: "Fair",
+        cleanliness: "Excellent",
+      },
       comments: "Great experience.",
-      createdAt: "2024-11-15",
     },
     {
       id: 2,
+      userName: "akfdls",
+      source: "paris",
+      destination: "tokyo",
+      date: "2024-11-12",
       starRating: 5,
-      punctuality: "Excellent",
-      comfort: "Excellent",
-      service: "Excellent",
-      foodAndBeverage: "Good",
-      cleanliness: "Excellent",
-      comments: "Would fly again.",
-      createdAt: "2024-11-12",
+      ratings: {
+        punctuality: "Excellent",
+        comfort: "Excellent",
+        service: "Excellent",
+        foodAndBeverage: "Good",
+        cleanliness: "Excellent",
+      },
+      comments:
+        "Would fly again. the food was really good and i enjoyed alot it was such a good experiece",
     },
   ];
 
@@ -33,33 +46,22 @@ const Feedback = () => {
   const averageRating = 4.5;
   const totalReviews = feedback.length;
 
-  // initial state of the filters
   const [starFilter, setStarFilter] = useState(null);
   const [categoryFilter, setCategoryFilter] = useState("");
 
   return (
     <div className="feedback-container">
-      <h1 className="page-title">Dashboard Overview</h1>
-
       {/* average rating and total no of reviews */}
-      <div className="feedback-stats">
-        <div className="feedback-stat">
-          <h2>Average Rating</h2>
-          <div className="feedback-rating">
-            <span>{averageRating.toFixed(1)}</span>
-            <StarIcon size={20} className="rating-star filled" fill="gold" />
-          </div>
-        </div>
-        <div className="feedback-stat">
-          <h2>Total Reviews</h2>
-          <span className="total-reviews">{totalReviews}</span>
-        </div>
+      <div>
+        <ReviewStatCard
+          totalReviews={totalReviews}
+          averageRating={averageRating}
+        />
       </div>
 
-      {/* filters section */}
+      {/* filter section */}
       <div className="feedback-filters">
         <div className="filter-group">
-          <h2 className="filter-header">Filter by Star Rating</h2>
           <div className="filter-buttons">
             {[1, 2, 3, 4, 5].map((rating) => (
               <button
@@ -74,15 +76,10 @@ const Feedback = () => {
                 {rating} <Star className="inline-icon" size={16} />
               </button>
             ))}
-            <button onClick={() => setStarFilter(null)} className="filter-btn">
-              <Filter size={16} />
-              <span>Clear</span>
-            </button>
           </div>
         </div>
 
         <div className="filter-group">
-          <h2 className="filter-header">Filter by Performance</h2>
           <select
             className="filter-dropdown"
             onChange={(e) => setCategoryFilter(e.target.value)}
@@ -102,39 +99,15 @@ const Feedback = () => {
       <div className="feedback-list">
         {feedback.map((item) => (
           <div key={item.id} className="feedback-card">
-            <div className="feedback-header">
-              <div className="feedback-stars">
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <Star
-                    key={index}
-                    size={20}
-                    className={index < item.starRating ? "filled" : "empty"}
-                    fill="currentColor"
-                  />
-                ))}
-              </div>
-              <p className="feedback-date">
-                {new Date(item.createdAt).toLocaleDateString()}
-              </p>
-            </div>
-            <p className="feedback-comment">{item.comments}</p>
-            <div className="feedback-categories">
-              <div>
-                <strong>Punctuality:</strong> {item.punctuality}
-              </div>
-              <div>
-                <strong>Comfort:</strong> {item.comfort}
-              </div>
-              <div>
-                <strong>Service:</strong> {item.service}
-              </div>
-              <div>
-                <strong>Food & Beverage:</strong> {item.foodAndBeverage}
-              </div>
-              <div>
-                <strong>Cleanliness:</strong> {item.cleanliness}
-              </div>
-            </div>
+            <ReviewCard
+              userName={item.userName}
+              source={item.source}
+              destination={item.destination}
+              date={item.date}
+              starRating={item.starRating}
+              ratings={item.ratings}
+              comments={item.comments}
+            />
           </div>
         ))}
       </div>
