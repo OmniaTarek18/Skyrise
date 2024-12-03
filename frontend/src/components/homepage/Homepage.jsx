3972791e43a28903d2dbf654f2fe2f08edc0e57f
nouranKhalil <nouran.ahraf.youssef@gmail.com>
2024-12-03 22:40:11 +0200

    Handle undefined components bugs

--- frontend/src/components/homepage/Homepage.jsx
+++ frontend/src/components/homepage/Homepage.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import Button from "../shared/Button";
 import { useNavigate } from "react-router-dom";
-import Nav from "../shared/Nav";
-import Promotion from "./Promotion";
-import promotionImg from "../../assets/promotion.jpeg"
-import Footer from "./Footer";
-import About from "./About";
+
 
 const Homepage = () => {
   const navigate = useNavigate();
   return (
     <>
-      <Nav />
-      <Promotion img ={promotionImg}/>
       <Button
         btnText={"Sign up"}
         btnColor="dark"
@@ -23,7 +17,7 @@ const Homepage = () => {
         btnColor="light"
         handleClick={() => navigate("/login")}
       />
-      <About/>
+      {/* <About/> */}
       {/* <Footer/> */}
     </>
   );
