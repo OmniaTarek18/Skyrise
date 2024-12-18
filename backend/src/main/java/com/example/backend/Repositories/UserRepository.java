bab9d4eaafe0889348f0fe40d4ee6b137cce4af9
Mohamed-Hammady <mohamedehab18127@gmail.com>
2024-12-18 06:13:28 +0200

    user add feedback to a flight

--- backend/src/main/java/com/example/backend/Repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/Repositories/UserRepository.java
@@ -1,12 +1,17 @@
 package com.example.backend.Repositories;
 
+import com.example.backend.Entities.Flight;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.example.backend.Entities.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findUserByUserId(Integer id);
 
 }
